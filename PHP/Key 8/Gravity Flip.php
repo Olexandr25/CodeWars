f37e7fe68a2751d482aea498cf3aae2c900ce52f
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2020-10-24 17:15:39 +0300

    New task Gravity Flip.php

---
+++ PHP/Key 8/Gravity Flip.php
@@ -0,0 +1,11 @@
+<?php
+function flip(string $dir, array $arr): array {
+    if ($dir == 'R'){
+        sort($arr);
+        return $arr;
+    }
+    else {
+        rsort($arr);
+        return $arr;
+    }
+}
